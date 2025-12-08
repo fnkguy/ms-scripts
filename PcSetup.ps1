30992daf0c1dd580d372f7a7ef40d103a591b738
Filipe Nepomuceno <fnepomuceno@microsoft.com>
2025-12-08 09:07:25 +0000

    update

--- PcSetup.ps1
+++ PcSetup.ps1
@@ -10,6 +10,16 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -value 0
 
 
+<# --- GIT --- removed since this needs to be created in order to get this script
+# Create git.repo folder in C:/
+New-Item -Path C:\git.repo
+# Change directory to C:\git.repo
+Set-Location C:\git.repo
+# Clone ms-sripts to git.repo folder
+git clone https://github.com/fnkguy/ms-scripts.git
+#>
+
+
 <# --- SOFTWARE --- #>
 # Package IDs to be installed
 $Packages = (
@@ -17,7 +27,6 @@ $Packages = (
     'KeePassXCTeam.KeePassXC'` #KeePass XC
     '9NZVDKPMR9RD'` #FireFox
     'Notepad++.Notepad++'` # NotePad++
-    'git.git'` #Git
     '9NBLGGH4Z1SP'` #ShareX
     'Microsoft.WindowsApp'` #Windows App
     'Microsoft.VisualStudioCode'` #Visual Studio Code
@@ -25,21 +34,13 @@ $Packages = (
     'Microsoft.WSL'` #WSL
     'google.chrome'` #Google Chrome
     '9PDXGNCFSCZV' ) #Ubunto
+    #'git.git'` #Git - removed since it needs to be installed in order to get this script
 
 
 # Install software
 foreach ($Package in $Packages){
     Winget install --id $Package --silent --accept-package-agreements
 }
-
-
-<# --- GIT --- #>
-# Create git.repo folder in C:/
-New-Item -Path C:\git.repo
-# Change directory to C:\git.repo
-Set-Location C:\git.repo
-# Clone ms-sripts to git.repo folder
-git clone https://github.com/fnkguy/ms-scripts.git
 
 
 <# --- POWERSHELL --- #>
