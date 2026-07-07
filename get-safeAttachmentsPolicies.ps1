ab5e1de4ceb6bcfb16d34b230484350c16d066d2
FunctionKey <filipenepopereira@gmail.com>
2026-07-07 16:16:42 +0100

    update

--- get-safeAttachmentsPolicies.ps1
+++ get-safeAttachmentsPolicies.ps1
@@ -1,33 +1,31 @@
-#OUTBOUND POLICIES & RULES
+#ANTI MALWARE POLICIES & RULES
 # select account
 $AdminAccount = Read-Host "Admin account"
 # Connect to Exchange Online PowerShell
 Connect-ExchangeOnline -UserPrincipalName $AdminAccount
 # Start transcript to log the output
-Start-Transcript -Path "C:\temp\OutboundPolicies.txt"
+Start-Transcript -Path "C:\temp\SafeAttachmentsPolicies.txt"
 # Get all outbound spam filter policies
-$outboundSpamPolicies = Get-HostedOutboundSpamFilterPolicy
+$SafeAttachmentsPolicies = Get-SafeAttachmentsPolicy
 # Get all outbound spam filter rules
-$outboundSpamRules = Get-HostedOutboundSpamFilterRule
+$SafeAttachmentsRules = Get-SafeAttachmentsRule
 # Display details of each policy
-foreach ($policy in $outboundSpamPolicies) {
+foreach ($policy in $SafeAttachmentsPolicies) {
 Write-Output "----------POLICIY $($policy.Name)-------------------"
 Write-Output "PolicyGuid           : $($policy.Guid)"
-Write-Output "Enabled              : $($policy.Enabled)"
-Write-Output "Default              : $($policy.Default)"
-Write-Output "External Limit - Hour: $($policy.RecipientLimitExternalPerHour)"
-Write-Output "Internal Limit - Hour: $($policy.RecipientLimitInternalPerHour)"
-Write-Output "Total Limit - Day    : $($policy.RecipientLimitPerDay)"
-Write-Output "Action               : $($policy.ActionWhenThresholdReached)"
-Write-Output "AutoForwarding       : $($policy.AutoForwarding)"
-Write-Output "Notify Outbound Spam : $($policy.NotifyOutboundSpamRecipients)"
-Write-Output "Notify               : $($policy.Notify)"
-Write-Output "Creation             : $($policy.Creation)"
-Write-Output "Change               : $($policy.Change)"
+Write-Output "Enabled              : $($policy.Enable)"
+Write-Output "Default              : $($policy.IsDefault)"
+Write-Output "Action               : $($policy.Action)"
+Write-Output "Blocking Encrypted   : $($policy.EnableBlockingEncryptedAttachments)"
+Write-Output "Organization Branding: $($policy.EnableOrganizationBranding)"
+Write-Output "Redirect             : $($policy.Redirect)"
+Write-Output "Redirect Address     : $($policy.RedirectAddress)"
+Write-Output "Creation UTC         : $($policy.WhenCreatedUTC)"
+Write-Output "Change UTC           : $($policy.WhenChangedUTC)"
 Write-Output "-------------------------------------"
 }
 # Loop through each rule to get detailed information
-foreach ($rule in $outboundSpamRules) {
+foreach ($rule in $SafeAttachmentsRules) {
 Write-Output "--------RULES $($filter.Name)------------------------"
 Write-Output "FilterGuid       : $($filter.Guid)"
 Write-Output "Priority         : $($filter.Priority)"
@@ -39,7 +37,7 @@ Write-Output "Groups Exception : $($filter.ExceptIfFromMemberOf)"
 Write-Output "Domains          : $($filter.SenderDomainIs)"
 Write-Output "Domains Exception: $($filter.ExceptIfSenderDomainIs)"
 Write-Output "Description      : $($filter.Description)"
-Write-Output "Change           : $($filter.WhenChanged)"
+Write-Output "Change           : $($filter.WhenChangedUTC)"
 Write-Host "-----------------------------"
 }
 # Stop transcript
